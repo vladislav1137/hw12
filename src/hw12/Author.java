c70738d64af21f9d1069176ef874129f80eb383b
vladislav1137 <mr.vladislav220@yandex.ru>
2023-03-15 22:46:12 +0300

    toString, equals and hashCode. v2

--- src/hw12/Author.java
+++ src/hw12/Author.java
@@ -1,5 +1,7 @@
 package hw12;
 
+import java.util.Objects;
+
 public class Author {
     private final String name;
    private final String surname;
@@ -19,5 +21,18 @@ public class Author {
 
     public String toString(){
         return this.name + this.surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
